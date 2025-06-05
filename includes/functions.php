5ff7de73eef31722c332a464a62fc171e2ed0f5e
Kim Coleman <kim@strangerstudios.com>
2025-06-05 14:18:35 -0400

    Fixing link to the how to make customizations blog post and making docs link https

--- includes/functions.php
+++ includes/functions.php
@@ -11,10 +11,10 @@ You want to be able to upgrade.
 If you were asked to insert code into "your functions.php file", it was meant that you edit the functions.php
 in the root folder of your active theme. e.g. /wp-content/themes/twentytwelve/functions.php
 You can also create a custom plugin to place customization code into. Instructions are here:
-http://www.paidmembershipspro.com/2012/08/create-a-plugin-for-pmpro-customizations/
+https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
 
 Further documentation for customizing Paid Memberships Pro can be found here:
-http://www.paidmembershipspro.com/documentation/
+https://www.paidmembershipspro.com/documentation/
  ****************************************************************/
 if ( ! function_exists( 'sornot' ) ) {
 	function sornot( $t, $n ) {
